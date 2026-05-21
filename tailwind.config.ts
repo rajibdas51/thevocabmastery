c4744bb26c0e0a3d502198b1b47f3b8697893696
Rajib chandra Das <prorajib6@gmail.com>
2026-05-21 14:43:48 +0600

    create tailwind config

---
+++ tailwind.config.ts
@@ -0,0 +1,10 @@
+import type { Config } from 'tailwindcss'
+import typography from '@tailwindcss/typography'
+
+const config: Config = {
+  // ... your existing config ...
+  plugins: [
+    typography,
+  ],
+}
+export default config
